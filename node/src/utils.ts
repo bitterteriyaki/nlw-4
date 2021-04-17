18d53870cb96b0b349abaa222a9e00241195748c
Caio Alexandre <contato.kyomi@gmail.com>
2021-04-17 17:19:25 -0300

    Primeira aula da NLW #4

---
+++ node/src/utils.ts
@@ -0,0 +1,62 @@
+// MIT License
+
+// Copyright (c) 2021 Caio Alexandre
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+const ColoursEnum = Object.freeze({
+  RESET: '\x1b[0m',
+  BRIGHT: '\x1b[1m',
+  DIM: '\x1b[2m',
+  UNDERSCORE: '\x1b[4m',
+  BLINK: '\x1b[5m',
+  REVERSE: '\x1b[7m',
+  HIDDEN: '\x1b[8m',
+
+  FOREGROUND: {
+      BLACK: '\x1b[30m',
+      RED: '\x1b[31m',
+      GREEN: '\x1b[32m',
+      YELLOW: '\x1b[33m',
+      BLUE: '\x1b[34m',
+      MAGENTA: '\x1b[35m',
+      CYAN: '\x1b[36m',
+      WHITE: '\x1b[37m'
+  },
+
+  BACKGROUND: {
+    BLACK: '\x1b[40m',
+    RED: '\x1b[41m',
+    GREEN: '\x1b[42m',
+    YELLOW: '\x1b[43m',
+    BLUE: '\x1b[44m',
+    MAGENTA: '\x1b[45m',
+    CYAN: '\x1b[46m',
+    WHITE: '\x1b[47m'
+  }
+});
+
+const RED = ColoursEnum.FOREGROUND.RED;
+const RESET = ColoursEnum.RESET;
+
+function log(...messages: string[]) {
+  console.log(`${RED}[app]${RESET}`, ...messages);
+}
+
+export { ColoursEnum, log };
